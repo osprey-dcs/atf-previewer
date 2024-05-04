818751a46514bf5a33a8ed3f02576d13e4af6a18
John Sinclair <jwsincl99999@gmail.com>
2024-05-03 20:49:00 -0400

    functions to support new file names and locations

---
+++ FileUtil.h
@@ -0,0 +1,29 @@
+//
+// Created by jws3 on 5/2/24.
+//
+
+#ifndef FILEREFORMATTER_FILEUTIL_H
+#define FILEREFORMATTER_FILEUTIL_H
+
+#include <QString>
+#include "Cnst.h"
+#include "DataHeader.h"
+
+class FileUtil {
+
+public:
+  FileUtil();
+  static bool hasExtension( QString str );
+  static QString getHeaderFileName (
+    const QString& rootDir,
+    const QString& name );
+  static QString extractFileName( QString str );
+  static QString getBinDir(
+     const QString& binRoot,
+     const QString& subDir );
+  static QString makeBinFileName( DataHeader *dh, const QString& hdrName, int sigIndex );
+
+};
+
+
+#endif //FILEREFORMATTER_FILEUTIL_H
