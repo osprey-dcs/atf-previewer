de2e3a97a6038ce129ab04ca3e4e0cc8c830bf5d
John Sinclair <jwsincl99999@gmail.com>
2024-05-09 21:40:13 -0400

    adding export dialog files

---
+++ ExportDialog.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jws3 on 5/9/24.
+//
+
+#include "ExportDialog.h"
---
+++ ExportDialog.h
@@ -0,0 +1,14 @@
+//
+// Created by jws3 on 5/9/24.
+//
+
+#ifndef VIEWER_EXPORTDIALOG_H
+#define VIEWER_EXPORTDIALOG_H
+
+
+class ExportDialog {
+
+};
+
+
+#endif //VIEWER_EXPORTDIALOG_H
