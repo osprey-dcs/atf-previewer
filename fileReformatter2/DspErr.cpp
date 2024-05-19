55946971c3937b219d46582ff1d2b0de8de71360
John Sinclair <jwsincl99999@gmail.com>
2024-05-19 17:31:54 -0400

    upgrading error reporting

---
+++ fileReformatter2/DspErr.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jws3 on 5/19/24.
+//
+
+#include "DspErr.h"
---
+++ fileReformatter2/DspErr.h
@@ -0,0 +1,14 @@
+//
+// Created by jws3 on 5/19/24.
+//
+
+#ifndef FILEREFORMATTER2_DSPERR_H
+#define FILEREFORMATTER2_DSPERR_H
+
+
+class DspErr {
+
+};
+
+
+#endif //FILEREFORMATTER2_DSPERR_H
