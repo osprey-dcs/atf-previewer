a4959c98519737569a3d914e2ee6252f1e0bc615
Michael Davidsaver <mdavidsaver@gmail.com>
2026-08-04 12:01:22 -0700

    cleanup FileUtil

--- fileReformatter2/FileUtil.cpp
+++ fileReformatter2/FileUtil.cpp
@@ -4,160 +4,17 @@
 
 #include "FileUtil.h"
 
-#include <utility>
+#include <QDir>
+#include <QFileInfo>
 
 FileUtil::FileUtil() = default;
 
-bool FileUtil::hasExtension( const QString& str ) {
-
-  /* Search backwards for a ".". If we find "/" first, then there is no extension.
-   * If we find a "." before finding a "/" and the "." is not the first character,
-   * then there is an extension.
-   */
-
-  // don't include 1st character in test
-  for ( int i=str.length()-1; i>0; i-- ) {
-    if ( str.at(i).toLatin1() == '/' ) return false;
-    if ( str.at(i).toLatin1() == '.' ) return true;
-  }
-
-  return false;
-
-}
-
-QString FileUtil::getHeaderFileName (
-  const QString& rootDir,
-  const QString& name ) {
-
-  /* Combine rootDir and name to get fullPath.
-   *
-   * If rootDir does not end with "/", append one.
-   * If name starts with a "./" or "/", then ignore rootDir.
-   * If name does not include an extension, append the default one.
-   */
-
-  QString path;
-
-  QString rootDir1 = rootDir.trimmed();
-  if ( !( rootDir1.endsWith( "/" ) ) ) {
-    rootDir1 += "/";
-  }
-
-  QString name1 = name.trimmed();
-  if ( !hasExtension( name1 ) ) {
-
-    if ( name1.endsWith( "." ) ) {
-      name1 += Cnst::HdrExtension.c_str();
-    }
-    else {
-      name1 += ".";
-      name1 += Cnst::HdrExtension.c_str();
-    }
-
-  }
-
-  if ( name1.startsWith( "./" ) || name1.startsWith( "/" ) ) {
-    path = name1;
-  }
-  else {
-    path = rootDir1 + name1;
-  }
-
-  return path;
-
-}
-
 QString FileUtil::extractFileName( const QString& str ) {
-
-  QString tmp = str;
-
-  if ( hasExtension( str ) ) {
-
-    int extensionStart = 0;
-
-    for ( int i=str.length()-1; i>0; i-- ) {
-      if ( str.at(i).toLatin1() == '.' ) {
-        extensionStart = i;
-        break;
-      }
-    }
-
-    if ( extensionStart > 0 ) {
-      tmp = str.mid( 0, extensionStart );
-    }
-
-  }
-
-  int dirEnd = 0;
-  for ( int i=tmp.length()-1; i>=0; i-- ) {
-    if ( tmp.at(i).toLatin1() == '/' ) {
-      dirEnd = i;
-      break;
-    }
-  }
-
-  if ( dirEnd && ( dirEnd != tmp.length()-1 ) ) {
-    tmp = tmp.mid( dirEnd+1 );
-  }
-
-  return tmp;
-
-}
-
-QString FileUtil::extractFileNameAndExt( QString str ) {
-
-  QString tmp;
-  tmp = std::move(str);
-
-  int dirEnd = 0;
-  for ( int i=tmp.length()-1; i>=0; i-- ) {
-    if ( tmp.at(i).toLatin1() == '/' ) {
-      dirEnd = i;
-      break;
-    }
-  }
-
-  if ( dirEnd && ( dirEnd != tmp.length()-1 ) ) {
-    tmp = tmp.mid( dirEnd+1 );
-  }
-
-  return tmp;
-
+  // "path/file.ext" -> "file"
+  return QFileInfo(str).completeBaseName();
 }
 
 QString FileUtil::extractDir( QString str ) {
-
-  QString tmp;
-  tmp = std::move(str);
-
-  int dirEnd = -1;
-  for ( int i=tmp.length()-1; i>=0; i-- ) {
-    if ( tmp.at(i).toLatin1() == '/' ) {
-      dirEnd = i;
-      break;
-    }
-  }
-
-  return tmp.left( dirEnd+1 );
-
-}
-
-QString FileUtil::getBinDir( const QString& binRoot, const QString& subDir ) {
-
-  QString binRoot1 = binRoot.trimmed();
-
-  if ( !( binRoot1.endsWith( "/" ) ) ) {
-    binRoot1 += "/";
-  }
-  binRoot1 += subDir.trimmed() + "/";
-
-  return binRoot1;
-
-}
-
-// Unused at the moment
-QString FileUtil::makeBinFileName( DataHeader *dh, const QString& hdrName, int sigIndex ) {
-
-  return "";
-
+  // "path/file.ext" -> "path"
+  return QFileInfo(str).dir().dirName() + "/";
 }
--- fileReformatter2/FileUtil.h
+++ fileReformatter2/FileUtil.h
@@ -13,17 +13,8 @@ class FileUtil {
 
 public:
   FileUtil();
-  static bool hasExtension( const QString& str );
-  static QString getHeaderFileName (
-    const QString& rootDir,
-    const QString& name );
   static QString extractFileName( const QString& str );
-  static QString extractFileNameAndExt( QString str );
   static QString extractDir( QString str );
-  static QString getBinDir(
-     const QString& binRoot,
-     const QString& subDir );
-  static QString makeBinFileName( DataHeader *dh, const QString& hdrName, int sigIndex );
 
 };
 
